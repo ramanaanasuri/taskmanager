b5a981add70ec643102b778998ea83a8e2b8cefb
Ramana Anasuri <ramanaanasuri@gmail.com>
2025-11-18 06:57:56 +0000

    script update for set env for aws

--- scripts/git_reset_for_aws_env.sh
+++ scripts/git_reset_for_aws_env.sh
@@ -3,5 +3,5 @@ git reset --hard HEAD
 echo "Remove all untracked files and folders"
 git clean -fdn
 git fetch origin
-git push origin feature/notifications-clean
+git pull origin feature/notifications-clean
 ./scripts/set_aws_env.sh
